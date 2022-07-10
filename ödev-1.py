66db3116beb04f00e114567e439cdd2e13f0e5c9
Mahmut <mahmut53celik@gmail.com>
2022-07-10 16:40:48 +0300

    Create ödev-1.py

---
+++ ödev-1.py
@@ -0,0 +1,24 @@
+# Görev 1:  Verilen değerlerin veri yapılarını inceleyiniz.Type() metodunu kullanınız.
+x = 8
+type(x)
+
+y = 3.2
+type(y)
+
+z = 8j + 18
+type(z)
+
+b = True
+type(b)
+
+c = 23 < 22
+type(c)
+
+l = [1, 2, 3, 4]
+type(l)
+
+d = {"Name":"Jake", "Age":27, "Adress":"Downtown" }
+type(d)
+
+s = {"Python", "Machine Learning", "Data Science"}
+type(s)
