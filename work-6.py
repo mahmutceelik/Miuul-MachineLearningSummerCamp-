19b59662e899d2ec59bf9da0d016f3e151dbb1a0
Mahmut <86982257+mahmutceelik@users.noreply.github.com>
2022-07-10 18:45:04 +0300

    Update work-6.py

--- work-6.py
+++ work-6.py
@@ -7,3 +7,7 @@ df.columns
 df.columns = ["NUM_" + col.upper() if df[col].dtype != "O" else col.upper() for col in df.columns]
 
 df.columns
+
+# Out = Index(['NUM_TOTAL', 'NUM_SPEEDING', 'NUM_ALCOHOL', 'NUM_NOT_DISTRACTED',
+#        'NUM_NO_PREVIOUS', 'NUM_INS_PREMIUM', 'NUM_INS_LOSSES', 'ABBREV'],
+#       dtype='object')
