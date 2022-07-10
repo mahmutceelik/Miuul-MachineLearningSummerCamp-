a23bd074ca9efc98cd2a82d64f4e998bf5a192c7
Mahmut <86982257+mahmutceelik@users.noreply.github.com>
2022-07-10 16:54:40 +0300

    Update work-2.py

--- work-2.py
+++ work-2.py
@@ -4,3 +4,17 @@
 text = "The goal is to turn data into information and information into insight"
 
 text.upper().split()
+
+# Out =
+['THE',
+ 'GOAL',
+ 'IS',
+ 'TO',
+ 'TURN',
+ 'DATA',
+ 'INTO',
+ 'INFORMATION',
+ 'AND',
+ 'INFORMATION',
+ 'INTO',
+ 'INSIGHT']
