62b64a0a07db9ac56c6c17af7faf16ff0b2e34e8
Mahmut <mahmut53celik@gmail.com>
2022-07-10 16:53:52 +0300

    Create work-2.py

---
+++ work-2.py
@@ -0,0 +1,6 @@
+# Görev 2: Verilen string ifadenin tüm harflerini büyük harfe çeviriniz.
+# Virgül ve nokta yerine space koyunuz,kelime kelime ayırınız.
+
+text = "The goal is to turn data into information and information into insight"
+
+text.upper().split()
