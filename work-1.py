d4bbd8344bad86a87e1709c6b06b2f371400cd17
Mahmut <86982257+mahmutceelik@users.noreply.github.com>
2022-07-10 16:45:40 +0300

    Update and rename ödev-1.py to work-1.py

--- work-1.py
+++ work-1.py
@@ -2,24 +2,32 @@
 
 x = 8
 type(x)
+# Out = int
 
 y = 3.2
 type(y)
+# Out = float
 
 z = 8j + 18
 type(z)
+# Out = complex
 
 b = True
 type(b)
+# Out = bool
 
 c = 23 < 22
 type(c)
+# Out = bool
 
 l = [1, 2, 3, 4]
 type(l)
+# Out = tuple
 
 d = {"Name":"Jake", "Age":27, "Adress":"Downtown" }
 type(d)
+# Out = dict
 
 s = {"Python", "Machine Learning", "Data Science"}
 type(s)
+# Out = Set
